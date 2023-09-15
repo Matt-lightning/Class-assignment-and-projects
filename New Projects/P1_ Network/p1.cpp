3e4bb620d0e2116a71d24bed30a3a5eea84dae44
Matt-lightning <swaggblazer2016@gmail.com>
2023-09-15 09:19:23 -0500

    Backing up files

--- New Projects/P1_ Network/p1.cpp
+++ New Projects/P1_ Network/p1.cpp
@@ -52,61 +52,138 @@ int main(int argc, char* argv[]){
         return 0;
     }
     
-    int i = 0;
+    int size = 0;
     
     string sources[1000];
     string dests[1000];
     string bws[1000];
     string unique[1000];
-    while (input >>  sources[i] >> dests[i] >> bws[i]){
+    string leafs[1000];
+    while (input >>  sources[size] >> dests[size] >> bws[size]){
        
 
         //cout << sources[i] << " " << dests[i] << " " << bws[i] << endl;
-        i++;
+        size++;
     }
     
-    // need every source and dest to be uppercase
-    for(int i= 0 ; i < 1000; i++){
+    // cout << "Sources" << endl;
+    // cout << "---------------------" << endl;
+    for(int i= 0 ; i < size; i++){
         if(sources[i] == ""){
             break;
         }
-        if(islower(sources[i][0])){
-            sources[i] = toupper(sources[i][0]);
+        for (long unsigned int j = 0; j < sources[i].size(); j++){
+            if(islower(sources[i][j])){
+                sources[i][j] = toupper(sources[i][j]);
+            }
         }
         
-        cout << sources[i] << endl;
+       // cout << sources[i] << endl;
         
     }
+    // cout << "---------------------" << endl;
+    // cout << "Dests" << endl;
 
-    cout << "---------------------" << endl;
+    // cout << "---------------------" << endl;
 
-    for(int i= 0 ; i < 1000; i++){
+    for(int i= 0 ; i < size; i++){
         if(dests[i] == ""){
             break;
         }
-        if(islower(dests[i][0])){
-            dests[i] = toupper(dests[i][0]);
+
+         for (long unsigned int j = 0; j < dests[i].size(); j++){
+            if(islower(dests[i][j])){
+                dests[i][j] = toupper(dests[i][j]);
+            }
         }
         
-        cout << dests[i] << endl;
+        // cout << dests[i] << endl;
         
     }
 
-    cout << "----------------------" << endl;
+    // cout << "---------------------" << endl;
+    // cout << "Uniques" << endl;
 
-    for(int i=0; i < sources[i].size(); i++){
-        for(int j=0; j < dests[j].size(); j++){
-            if(sources[i] == dests[j]){
-                unique[i] = sources[i];
+    // cout << "----------------------" << endl;
+
+    int uniqueSize = 2;
+    unique[0] = sources[0];
+    unique[1] = dests[0];
+    for(int i=1; i < size; i++){ // This loop sorts through the sources and dests and adds them to the unique array
+        bool here = false;
+         
+        for(int j=1; j < size; j++){ // This loop checks if the source and dests is already in the unique array
+            if(sources[i] == unique[j]){
+                here = true;
             }
-            else if(sources[i] != sources[i+1]){
-                unique[i] = sources[i];
-            }   
-            else if(dests[j] != dests[j+1]){
-                unique[i] = dests[j];
             }
+            if(here == false){
+                unique[uniqueSize] = sources[i];
+                uniqueSize++;
+            } 
+            here = false;
+            for(int j=1; j < size; j++){ // This loop checks if the source and dests is already in the unique array
+            if(dests[i] == unique[j]){
+                here = true;
+            }
+            }
+            if(here == false){
+                unique[uniqueSize] = dests[i];
+                uniqueSize++;
+            }
+
         }
+        //cout << unique[i] << endl;
+    
+
+    //cout << "here" << endl;
+
+    for (int i = 0; i < uniqueSize; i++) {
         cout << unique[i] << endl;
+    }
+    cout << endl;
+    cout << endl;
+
+    // //leafs
+    int leafSize = 0;
+    // for(int i=0; i < size; i++){
+    //     // if(leafs[i] == ""){
+    //     //     break;
+    //     // }
+    //     for( int j=0; j < size; j++){
+    //         if(unique[i] == sources[j] && unique[i] == dests[j]){
+    //             leafs[leafSize] = unique[i];
+    //             leafSize++;
+    //         }
+    //     }
+    // }
+    for(int i=1; i < size; i++){ 
+        bool here = false;
+         
+        for(int j=1; j < size; j++){ 
+            if(leafs[i] == unique[j]){
+                here = true;
+            }
+            }
+            if(here == false){
+                unique[uniqueSize] = sources[i];
+                uniqueSize++;
+            } 
+            here = false;
+            for(int j=1; j < size; j++){ 
+            if(dests[i] == unique[j]){
+                here = true;
+            }
+            }
+            if(here == false){
+                unique[uniqueSize] = dests[i];
+                uniqueSize++;
+            }
+
+        }
+
+    for (int i = 0; i < leafSize; i++) {
+        cout << leafs[i] << endl;
     }
 
     input.close();
